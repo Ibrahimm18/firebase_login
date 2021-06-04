f2fe99f6b70e6d837bc12a98796768f58dee059a
Mohammed Ibrahim <8807290200mib@gmail.com>
2021-06-04 17:11:20 +0530

    updated1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//components
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+//Guards
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from "@angular/fire/auth-guard";
+
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['signin']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['']);
+const routes: Routes = [
+  
+    { path: 'signin', 
+    component: SigninComponent,        
+    canActivate: [AngularFireAuthGuard], 
+    data: { authGuardPipe: redirectLoggedInToHome },
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AngularFireAuthGuard],
+    data:  { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: '**',
+    component: PagenotfoundComponent,
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDk_PlzLI4gflwK5Pv5hNz5GkQhWQWd8ps",
+    authDomain: "logingit-b9efc.firebaseapp.com",
+    projectId: "logingit-b9efc",
+    storageBucket: "logingit-b9efc.appspot.com",
+    messagingSenderId: "104129821668",
+    appId: "1:104129821668:web:ded3ee78e037275829a5f1"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
